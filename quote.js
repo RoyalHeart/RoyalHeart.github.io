84f4d614355616c06a7025aee85e99d668d3fa62
Tam Thai <hoangtam3062002@gmail.com>
2023-02-03 21:48:03 +0700

    add resume download back

--- quote.js
+++ quote.js
@@ -1,17 +1,17 @@
-let url = "https://type.fit/api/quotes";
+const url = "https://type.fit/api/quotes";
 fetch(url)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    let random = Math.floor(Math.random() * data.length);
-    let randomQuote = data[random];
-    let quote = randomQuote.text;
-    let author = randomQuote.author;
+    const random = Math.floor(Math.random() * data.length);
+    const randomQuote = data[random];
+    const quote = randomQuote.text;
+    const author = randomQuote.author;
     if (!author) {
       author = "Anonymous";
     }
-    let quoteText = document.getElementsByName("randomQuote");
+    const quoteText = document.getElementsByName("randomQuote");
     for (let i = 0; i < quoteText.length; i++) {
       quoteText[i].innerHTML = quote + " - " + author;
     }
