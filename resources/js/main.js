239d6136237c77cf3c40ef32b518de3b47718271
Tam Thai <hoangtam3062002@gmail.com>
2023-11-13 09:41:30 +0700

    Update projecct, fix css

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,20 +11,33 @@ container.addEventListener("wheel", function (e) {
     e.preventDefault();
   }
 });
-var autoScrollTimer = setInterval(() => {
-  let i = 0;
-  let before = container.scrollLeft;
-  var a = setInterval(() => {
-    before = container.scrollLeft;
-    container.scrollLeft += 16;
-    i++;
-    if (i >= 20) {
-      window.clearInterval(a);
-    }
-    if (container.scrollLeft <= before) {
-      setTimeout(() => {
-        container.scrollLeft = 0;
-      }, 2000);
-    }
-  }, 20);
-}, 3000);
+const project_container = document.getElementsByClassName("project-content")[0];
+// where "container" is the id of the container
+project_container.addEventListener("wheel", function (e) {
+  if (e.deltaY > 0) {
+    project_container.scrollLeft += 220;
+    e.preventDefault();
+    // prevenDefault() will help avoid worrisome
+    // inclusion of vertical scroll
+  } else {
+    project_container.scrollLeft -= 220;
+    e.preventDefault();
+  }
+});
+// var autoScrollTimer = setInterval(() => {
+//   let i = 0;
+//   let before = container.scrollLeft;
+//   var a = setInterval(() => {
+//     before = container.scrollLeft;
+//     container.scrollLeft += 16;
+//     i++;
+//     if (i >= 20) {
+//       window.clearInterval(a);
+//     }
+//     if (container.scrollLeft <= before) {
+//       setTimeout(() => {
+//         container.scrollLeft = 0;
+//       }, 2000);
+//     }
+//   }, 20);
+// }, 3000);
